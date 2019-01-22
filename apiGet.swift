96ab18ffd2d84c1c9b6ea6e45c550e4fe07c0583
mohamedreda1993 <45246611+mohamedreda1993@users.noreply.github.com>
2019-01-22 21:49:47 +0200

    Create apiGet.swift

---
+++ apiGet.swift
@@ -0,0 +1,29 @@
+//
+//  apiGet.swift
+//  Api`s
+//
+//  Created by Mohamed reda on 8/23/18.
+//  Copyright © 2018 fci. All rights reserved.
+//
+import Foundation
+
+struct apiGet : Codable{
+    
+    let id:Int
+    let name:String
+    let img:String
+    let location:String
+    let phone:String
+    let rate:Float
+    let foods:[foods]
+}
+
+struct foods : Codable{
+    
+    let id:Int
+    let name:String
+    let content:String
+    let salary:String
+    let image:String
+    let rate:Float
+}
